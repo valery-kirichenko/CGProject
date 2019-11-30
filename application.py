c928c2ecc58f9fe77cef796dafea75c592e898f0
Valery Kirichenko <valera5505@gmail.com>
2019-11-30 23:06:53 +0300

    RIP Image recognition

--- application.py
+++ application.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, render_template, request
-from keras.models import load_model
-from PIL import Image
-import re
-from io import BytesIO
-import base64
-from keras.preprocessing.image import img_to_array
+# from keras.models import load_model
+# from PIL import Image
+# import re
+# from io import BytesIO
+# import base64
+# from keras.preprocessing.image import img_to_array
 
 
-model = load_model('mnist99.h5')
-model._make_predict_function()
+#model = load_model('mnist99.h5')
+#model._make_predict_function()
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -22,13 +22,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/recognise", methods=["POST"])
-def recognise():
-    rq_image = request.get_json()['image']
-    image_data = base64.b64decode(re.sub('^data:image/.+;base64,', '', rq_image))
-    image = Image.open(BytesIO(image_data)).convert('L')
-    img_array = img_to_array(image)
-    img_array.astype('float32')
-    img_array /= 255
-    return str(model.predict_classes(img_array.reshape(1, 784))[0])
+# @app.route("/recognise", methods=["POST"])
+# def recognise():
+#     rq_image = request.get_json()['image']
+#     image_data = base64.b64decode(re.sub('^data:image/.+;base64,', '', rq_image))
+#     image = Image.open(BytesIO(image_data)).convert('L')
+#     img_array = img_to_array(image)
+#     img_array.astype('float32')
+#     img_array /= 255
+#     return str(model.predict_classes(img_array.reshape(1, 784))[0])
 
